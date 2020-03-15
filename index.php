13bef928211c2a2fa9eb3b7cad065340300fccfa
Mikhail Gusev <king-tut@yandex.ru>
2020-03-16 01:19:01 +0300

    Добавления SESSION и доступ к action.php

--- index.php
+++ index.php
@@ -1,16 +1,38 @@
 <?php 
 error_reporting(E_ALL);
+session_start();
 
 $setConnection = "host=localhost port=5432 dbname=mydb user=admin password=admin";
 $dbConnection = pg_connect($setConnection);
 
+if (isset($_POST['exit'])) {
+    unset($_SESSION['loginId']);
+    session_destroy();
+}
+
+if (parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) == "/action/" && !isset($_SESSION['loginId'])) {
+    header('location: http://'.$_SERVER['HTTP_HOST'].'/', true, 301);
+}
+
+if (isset($_POST['action'])) {
+    $actionDate = date("Y-m-d H:i:s", time());
+    $userid = $_SESSION['loginId'];
+    $pgResult = pg_query($dbConnection, "INSERT INTO actions_user (user_id, action_date) VALUES ('$userid', '$actionDate')");
+    if($pgResult) {
+        $massege = " Событие успешно добавленно";
+    } else {
+        $massege = "Произошла ошибка";
+    }
+}
+
 if (isset($_POST['enter'])) {
     $login = trim($_POST['login']);
     $password = trim($_POST['password']);
-    $pgResult = pg_query($dbConnection, "SELECT login, password FROM users WHERE login = '$login'");
+    $pgResult = pg_query($dbConnection, "SELECT id, login, password FROM users WHERE login = '$login'");
     $pgData = pg_fetch_assoc($pgResult);
     if(pg_num_rows($pgResult)) {
         if ($pgData['login'] == $login && $pgData['password'] == $password) {
+            $_SESSION['loginId'] = $pgData['id'];
             header('location: http://'.$_SERVER['HTTP_HOST'].'/action/', true, 301);
         } else {
             $massege = "Вы ввели не равильный пароль";
--- template/action.php
+++ template/action.php
@@ -2,9 +2,11 @@
     require_once $_SERVER['DOCUMENT_ROOT'] . "/template/header.php";
 ?>
 
-    <form action="/" method="post">
+    <form action="/action/" method="post">
         <input class="btn_action" type="submit" name="action" value="Добавить событие">
+        <input class="btn_exit" type="submit" name="exit" value="Выйти">
     </form>
+    <div class="result"><?= $massege ?? " " ?></div>
 
 <?php 
     require_once $_SERVER['DOCUMENT_ROOT'] . "/template/footer.php";
